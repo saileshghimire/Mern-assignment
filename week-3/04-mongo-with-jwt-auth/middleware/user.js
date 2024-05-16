900be337feffc19ce2ee4128c1482975659b9bb9
saileshghimire <saileshghimire1@gmail.com>
2024-05-16 10:57:55 +0545

    user middleware done

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,17 @@
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const token = req.headers.authorization;
+    const words = token.split(" ");
+    const jwttoken = words[1];
+    const decodedValue = jwt.verify(jwttoken,secret);
+    if(decodedValue.username){
+        next();
+    } else {
+        res.status(403).json({
+            message:"You are not autenticated"
+        });
+    }
 }
 
 module.exports = userMiddleware;
