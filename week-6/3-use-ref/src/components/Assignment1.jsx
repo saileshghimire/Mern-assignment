602589ac2c74e51713d04ee3e4609ee045f0741a
saileshghimire <saileshghimire1@gmail.com>
2024-05-28 10:07:35 +0545

    useRef assignment 1 complete

--- week-6/3-use-ref/src/components/Assignment1.jsx
+++ week-6/3-use-ref/src/components/Assignment1.jsx
@@ -1,20 +1,26 @@
+import { useRef } from "react";
 import { useEffect } from "react";
 
-// Create a component with a text input field and a button. When the component mounts or the button is clicked, automatically focus the text input field using useRef.
+// Create a component with a text input field and a button.
+// When the component mounts or the button is clicked, automatically focus the text input field using useRef.
 
 export function Assignment1() {
-
+    const inputRef = useRef();
     useEffect(() => {
+        inputRef.current.focus();
+    }, [inputRef]);
 
-    }, []);
-
+    // const handleButtonClick = () => {
+    //     document.getElementById("inputBox").focus()
+    // };
+    
     const handleButtonClick = () => {
-
+        inputRef.current.focus();
     };
 
     return (
         <div>
-            <input type="text" placeholder="Enter text here" />
+            <input ref={inputRef} id="inputBox" type="text" placeholder="Enter text here" />
             <button onClick={handleButtonClick}>Focus Input</button>
         </div>
     );
